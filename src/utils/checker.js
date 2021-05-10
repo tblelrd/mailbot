69d5b8ddd98a672c3bece1a6f5363d8959458f05
Jack <getjacked540c@gmail.com>
2021-05-10 17:49:30 +0100

    testg

--- src/utils/checker.js
+++ src/utils/checker.js
@@ -25,9 +25,7 @@ const checker = (msg, bot, seenMails) => {
             let newMails = [];
             if(seenMailsList) newMails = mails.filter(mail => seenMailsList.includes(mail));
 
-            console.log(seenMailsList.map(mail => mail.title));
-            console.log(newMails.map(mail => mail.title));
-            if(!newMails.length == 0) return;
+            if(newMails.length == 0) return;
 
             msg.channel.send(`You've got ${newMails.length} new ${newMails.length > 1 ? 'mails' : 'mail'}! \nType [read] to read your mail.`);
         });
